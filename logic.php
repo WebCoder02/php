e806c73aa787d5eb45bb5d9104e0fc4c5c00f740
Web_Coder_Anuj <91132523+WebCoder02@users.noreply.github.com>
2025-05-26 17:04:28 +0530

    Create logic.php

---
+++ logic.php
@@ -0,0 +1,16 @@
+<?php
+$a = true;
+$b = false;
+
+echo "a is : true<br/>";
+echo "b is : false<br/>";
+
+echo "a AND b: ";
+var_dump($a && $b);
+echo "<br/>";
+
+echo "a OR b: ";
+var_dump($a || $b);
+
+
+?>
